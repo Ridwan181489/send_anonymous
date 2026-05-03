08e7582e3da2bd097dffd8ec193933101b8b7055
MD. RIDWAN RAHMAN RADHI <it55151805@gmail.com>
2026-05-04 01:34:40 +0600

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
+  "https://ltjbusdajhrjtcqthffb.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx0amJ1c2RhamhyanRjcXRoZmZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4MTkyMjMsImV4cCI6MjA5MzM5NTIyM30.r6BYVyQXuSxBIgvZYyVcDzgki4ZlL8jIh-kGyH66i8I"
+"
 );
 const sb = window.supabase.createClient(SUPA_URL, SUPA_KEY);
 
